e049ccb53cc1e140f6e435648020316a5d76440d
fluoz <hasanabdulah31959@gmail.com>
2020-12-23 17:30:34 +0700

    post data

--- app.js
+++ app.js
@@ -27,7 +27,26 @@ app.get('/trans', (req , res) => {
 });
 
 app.post('/trans', (req, res) => {
-  res.json(req.body);
+   let sql = 'INSERT INTO transaction(fromUser, toUser ,amount, price) VALUES (?)';
+   let values = [
+     req.body.fromUser,
+     req.body.toUser,
+     req.body.amount,
+     req.body.price,
+   ];
+
+   db.query(sql,[values],function (error){
+     if(error){
+       console.log(error)
+     }
+     else {
+       res.json({
+         "status": 201,
+         "data": req.body
+       })
+     }
+   });
+
 });
 
 app.listen(port, () => {
